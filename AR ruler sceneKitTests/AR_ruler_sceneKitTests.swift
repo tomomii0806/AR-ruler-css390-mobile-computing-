85407882696c017dc2c391f715bd6165a4e04f91
tomomii0806 <dlcms4423@gmail.com>
2020-11-23 21:44:13 -0800

    Initial Commit

---
+++ AR ruler sceneKitTests/AR_ruler_sceneKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  AR_ruler_sceneKitTests.swift
+//  AR ruler sceneKitTests
+//
+//  Created by 中村友美 on 11/23/20.
+//
+
+import XCTest
+@testable import AR_ruler_sceneKit
+
+class AR_ruler_sceneKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
